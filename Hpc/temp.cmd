3dec2a20f8cd573bfa3e78da07cdf284a180ce14
Till Weigt <till_weigt@yahoo.de>
2020-01-04 17:46:46 +0100

    push

---
+++ Hpc/temp.cmd
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set the number of CPU cores per node
+#SBATCH --ntasks-per-node 72
+
+# How much memory is needed (per node). Possible units: K, G, M, T
+#SBATCH --mem=50G
+
+# set a partition
+#SBATCH --partition express
+
+# set max wallclock time
+#SBATCH --time=0-02:00:00
+
+# set name of job
+#SBATCH --job-name=test
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# set an output file
+#SBATCH --output Output/Output
+
+# send mail to this address
+#SBATCH --mail-user=till.weigt@gmail.com
+
+# run the application
+../julia-1.1.0/bin/julia \
+Hpc/temp.jl
