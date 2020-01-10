8ae4d3014cbdd29efaae6ea991590b16a4ce416c
Till Weigt <till_weigt@yahoo.de>
2020-01-10 08:43:04 +0100

    push

---
+++ Hpc/Mcmc_100RepOf1.cmd
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set the number of CPU cores per node
+#SBATCH --ntasks-per-node 72
+
+# How much memory is needed (per node). Possible units: K, G, M, T
+#SBATCH --mem=50G
+
+# set a partition
+#SBATCH --partition normal
+
+# set max wallclock time
+#SBATCH --time=7-00:00:00
+
+# set name of job
+#SBATCH --job-name=IbisData_100RepOf1
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# set an output file
+#SBATCH --output Output/Console/%x_%A
+
+# send mail to this address
+#SBATCH --mail-user=till.weigt@gmail.com
+
+# run the application
+../julia-1.1.0/bin/julia \
+src/main.jl \
+NParallel \
+WellLog \
+128 `#NumberOfStateParticle` \
+20000 `#NumberOfMcmcStep` \
+1 `#NumberOfParameterParticle` \
+1 `#PrintEach` \
+false `#CovarianceScaling` \
+true `#McmcFullCovariance` \
+5000 `#McmcUpdateIntevalLength` \
+10000 `#McmcLstUpdateIndex` \
+0.001 `#McmcVarianceInitialisation` \
+1.1 `#ResampleThresholdIbis` \
+1 `#NumberOfDensityPoint` \
+true `#SaveOutput` \
+Mcmc `#AlgorithmType` \
+100 `#ComputationLoopNumber`
