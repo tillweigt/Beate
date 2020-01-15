02bd8614507b9ec6f9728fc2b9b97a977ae3352b
Unknown <till_weigt@yahoo.de>
2020-01-15 11:49:23 +0100

    push

--- src/main.jl
+++ src/main.jl
@@ -90,22 +90,7 @@ for preRun in 1:5
 end
 
 computationLoopNumber = 1
-# for computationLoopNumber in 1:parse(Int64, Args[16])
-
-	Args[3] = "128" # NumberOfStateParticle = 128,
-	Args[4] = "5000" # NumberOfMcmcStep = 1,
-	Args[5] = "1" # NumberOfParameterParticle = 50,
-	Args[6] = "100" # PrintEach = 1,
-	Args[7] = "false" # CovarianceScaling = false,
-	Args[8] = "true" # McmcFullCovariance = true,
-	Args[9] = "500" # McmcUpdateIntervalLength = 500,
-	Args[10] = "1000" # McmcLastUpdateIndex = 1000,
-	Args[11] = "0.001" # McmcVarianceInitialisation = 0.001,
-	Args[12] = "1.1" # ResampleThresholdIbis = 1.1,
-	Args[13] = "1" # NumberOfDensityPoint = 10,
-	Args[14] = "false" # SaveOutput = true
-	Args[15] = "Mcmc"
-	Args[16] = "1"
+for computationLoopNumber in 1:parse(Int64, Args[16])
 
 	Output = run_Algorithm(
 		Model,
@@ -133,4 +118,4 @@ computationLoopNumber = 1
 		Symbol(Args[15]) # AlgorithmType
 	)
 
-# end
+end
