542cb61a67f745f59a2f6dda125f2cc7940eb9a8
Unknown <till_weigt@yahoo.de>
2020-01-26 13:18:47 +0100

    push

--- Hpc/IbisDataSim_manyOfMany.cmd
+++ Hpc/IbisDataSim_manyOfMany.cmd
@@ -10,10 +10,10 @@
 #SBATCH --mem=50G
 
 # set a partition
-#SBATCH --partition d0ow
+#SBATCH --partition requeue
 
 # set max wallclock time
-#SBATCH --time=7-00:00:00
+#SBATCH --time=0-02:00:00
 
 # set name of job
 #SBATCH --job-name=IbisDataSim_manyOfMany
@@ -33,7 +33,7 @@ Parallel \
 WellLogManyOfMany \
 128 `#NumberOfStateParticle` \
 1 `#NumberOfMcmcStep` \
-500 `#NumberOfParameterParticle` \
+1000 `#NumberOfParameterParticle` \
 1 `#PrintEach` \
 false `#CovarianceScaling` \
 true `#McmcFullCovariance` \
